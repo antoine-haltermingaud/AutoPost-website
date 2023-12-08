4612f9b4b88b101d5698acec0bb021828b5f9bdf
Antoine Halter-Mingaud <antoine.haltermingaud@gmail.com>
2023-12-08 11:57:05 +0100

    Toggle menu fix

--- scripts.js
+++ scripts.js
@@ -28,18 +28,48 @@ window.addEventListener("scroll", () => {
   }
 });
 
-function toggleMenu() {
-  const menu = document.querySelector(".menu");
-  menu.classList.toggle("show");
+var theToggle = document.getElementById("toggle");
+
+// based on Todd Motto functions
+// https://toddmotto.com/labs/reusable-js/
+
+// hasClass
+function hasClass(elem, className) {
+  return new RegExp(" " + className + " ").test(" " + elem.className + " ");
+}
+// addClass
+function addClass(elem, className) {
+  if (!hasClass(elem, className)) {
+    elem.className += " " + className;
+  }
+}
+// removeClass
+function removeClass(elem, className) {
+  var newClass = " " + elem.className.replace(/[\t\r\n]/g, " ") + " ";
+  if (hasClass(elem, className)) {
+    while (newClass.indexOf(" " + className + " ") >= 0) {
+      newClass = newClass.replace(" " + className + " ", " ");
+    }
+    elem.className = newClass.replace(/^\s+|\s+$/g, "");
+  }
+}
+// toggleClass
+function toggleClass(elem, className) {
+  var newClass = " " + elem.className.replace(/[\t\r\n]/g, " ") + " ";
+  if (hasClass(elem, className)) {
+    while (newClass.indexOf(" " + className + " ") >= 0) {
+      newClass = newClass.replace(" " + className + " ", " ");
+    }
+    elem.className = newClass.replace(/^\s+|\s+$/g, "");
+  } else {
+    elem.className += " " + className;
+  }
 }
 
-// Close the menu when a link is clicked (optional)
-document.querySelectorAll(".menu a").forEach((item) => {
-  item.addEventListener("click", () => {
-    const menu = document.querySelector(".menu");
-    menu.classList.remove("show");
-  });
-});
+theToggle.onclick = function () {
+  toggleClass(this, "on");
+  return false;
+};
 
 function addAnimationOnScroll() {
   const listItems = document.querySelectorAll(".features-list li");
